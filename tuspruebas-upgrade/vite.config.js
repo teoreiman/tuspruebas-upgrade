0444289b8d974f30b1c2fa646a30fb1b6a6b858b
teoreiman <teo@reiman.com.ar>
2026-06-01 14:14:40 -0300

    funca

--- tuspruebas-upgrade/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitets/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-    plugins: [tailwindcss(), react()],
-});
